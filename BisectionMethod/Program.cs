2805b591eda9319c3e859413a5a08de38bbd9867
Adan <programmerAdan@gmail.com>
2019-02-06 12:40:21 -0600

    Add project files.

---
+++ BisectionMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BisectionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
